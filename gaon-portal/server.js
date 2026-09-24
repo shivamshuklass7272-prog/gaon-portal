376d56934d942455473febda6182667f61fab93b
shivamshukla7272-prog <shivamshuklass7272@gmail.com>
2026-09-24 18:45:31 +0530

    Add robots.txt and sitemap.xml routes for Google Search SEO

--- gaon-portal/server.js
+++ gaon-portal/server.js
@@ -210,6 +210,25 @@ app.use((req, res, next) => {
     next();
 });
 
+app.get('/robots.txt', (req, res) => {
+    res.type('text/plain');
+    res.send(`User-agent: *
+Allow: /
+Sitemap: https://bastigaonportal.onrender.com/sitemap.xml`);
+});
+
+app.get('/sitemap.xml', (req, res) => {
+    res.type('application/xml');
+    res.send(`<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  <url>
+    <loc>https://bastigaonportal.onrender.com/</loc>
+    <changefreq>daily</changefreq>
+    <priority>1.0</priority>
+  </url>
+</urlset>`);
+});
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Database readiness check middleware
